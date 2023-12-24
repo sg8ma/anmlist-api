17bd8b39ccf71c638c4012dd300d1a3ce04ae6f5
daichisugiyama <daichi.sugiyama.public@gmail.com>
2023-12-24 15:12:28 +0900

    fix: api route only

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/user', [UserController::class, 'list']);
+Route::get('/user/{id}', [UserController::class, 'read']);
+Route::post('/user', [UserController::class, 'create']);
+Route::put('/user/{id}', [UserController::class, 'update']);
+Route::delete('/user/{id}', [UserController::class, 'delete']);
